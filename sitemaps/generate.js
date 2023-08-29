0823b9461b2f6aaa6388e8af805b0d23aec59bfe
sasi.eth <me@sasi.codes>
2023-08-29 17:46:18 +0530

    feat: update sitemaps

---
+++ sitemaps/generate.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+
+const fileName = "3";
+const maxItems = 50000
+const profiles = fs
+    .readFileSync(`profiles.csv`, "utf-8")
+    .split("\n")
+    .filter(Boolean);
+
+const xmlHeader = '<?xml version="1.0" encoding="UTF-8"?>\n';
+const urlsetHeader =
+    '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
+const urlsetFooter = "\n</urlset>\n";
+
+let sitemapXml = xmlHeader + urlsetHeader;
+
+profiles.map((handle, index) => {
+    if (index < maxItems) {
+        sitemapXml += `${index === 0 ? "" : "\n"}\t<url>
+        <loc>https://lenstube.xyz/channel/${handle.replace('.lens', '')}</loc>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+      </url>`;
+    }
+});
+
+sitemapXml += urlsetFooter;
+
+fs.writeFileSync(`channels/${fileName}.xml`, sitemapXml, "utf-8");
